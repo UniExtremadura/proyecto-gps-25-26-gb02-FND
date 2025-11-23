e97cc060146133ea1b6588a7f258a6082dd8e242
PiporGames <pipornet@gmail.com>
2025-11-23 20:24:14 +0100

    SCRUM-94 ST-47 Limpiar código obsoleto

--- static/_old/oldjs/footer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("/footer")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("footer-placeholder").innerHTML = data;
-            document.dispatchEvent(new Event("footerLoaded")); // Evento para dark-mode
-        })
-        .catch(error => console.error('Error al cargar el footer:', error));
-});
